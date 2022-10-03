eb57a9a8bf6c321d67c17d35ea048bfe556a5d74
RoniTuohimaa <t1turo00@students.oamk.fi>
2022-10-03 12:12:55 +0300

    viikkotehtävä5

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [age,setAge] = useState(0)
+  const [yläsyke,setYläsyke] = useState(0)
+  const [alasyke,setAlasyke] = useState(0)
+  
+
+  function calculate() {
+   setYläsyke((200-age)*0.85)
+   setAlasyke((200-age)*0.65)
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="cointainer">
+    <h3>Heart rate limits calculator</h3>
+    <form>
+      <div>
+        <label>Age</label>
+        <input type="number" value={age} onChange={e => setAge(e.target.value)}/>
+      </div>
+      <div>
+        <label>Heart rate limits</label>
+        <output type="block"> value={alasyke.toFixed(0)}-{yläsyke.toFixed(0)} </output>
+      </div>
+        <button type="button" onClick={calculate}>Calculate</button>
+        </form> 
     </div>
+    
   );
 }
 
